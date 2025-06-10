931596780abf1544b91d666ec62f854a242e8199
Filipe Chau <filipechau.dev@gmail.com>
2025-06-10 20:53:25 +0200

    add user input examples

---
+++ 03-user-input/examples.py
@@ -0,0 +1,9 @@
+# User input in python
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+age = age + 1
+print(f"Hello {name}, you are {age} years old.")
+print(f"You are {age} years old")
+      
