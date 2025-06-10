20044a172ec7578f2bf2acf3301e5f147f466d06
Filipe Chau <filipechau.dev@gmail.com>
2025-06-10 20:36:01 +0200

    add initial examples for typecasting

---
+++ 02-type-casting/examples.py
@@ -0,0 +1,17 @@
+# Typecasting is the process of converting one data type to another.
+# In Python, typecasting can be done using built-in functions like int(), float(), str(), etc.
+
+# There are two types of typecasting: Explicit and Implicit.
+# Explicit typecasting is when you manually convert a data type to another using built-in functions.
+# Implicit typecasting is when Python automatically converts one data type to another.
+
+# Example of Explicit Typecasting
+name = "Chau"
+age = 21
+gpa = 4.9
+student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
