bfad2148f528b62cd131488cb347473411ac1c39
Filipe Chau <filipechau.dev@gmail.com>
2025-06-10 20:19:46 +0200

    add improve readability for the examples in example.py

--- 01-variables/examples.py
+++ 01-variables/examples.py
@@ -15,13 +15,14 @@ quantity = 4
 
 # Float
 price = 17.50
-gpa = 5.75
+gpa = 10.75
 distance = 100.5
-print(f"The price is {price} and the quantity is {quantity}")
+print(f"The price is {price} MZN and the quantity is {quantity}")
 print(f"{first_name} {last_name} is {age} years old and has a GPA of {gpa}")
 
 # Boolean
-is_student = True
-is_employed = False
+is_student = False
+is_employed = True
 
 print(f"Is {first_name} a student? {is_student}")
+print(f"Is {first_name} employed? {is_employed}")
