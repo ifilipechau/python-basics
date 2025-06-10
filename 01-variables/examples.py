13f9c1f306fe1727cc0cbca0b45f607ba58b3716
Filipe Chau <filipechau.dev@gmail.com>
2025-06-10 20:00:32 +0200

    Refactor examples.py to improve variable organization and add comments for clarity

--- 01-variables/examples.py
+++ 01-variables/examples.py
@@ -1,11 +1,19 @@
 # Variables 
 
 
-# The variables below are called strings, they are being used with an f string
 
+# Strings
 first_name = "Filipe"
 last_name = "Chau"
 email = "chaulee@real.com"
 print(f"Hello, {first_name} {last_name}!")
 print(f"Your email address is: {email}")
 
+# Integers
+age = 25
+num_of_students = 30
+quantity = 4
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
