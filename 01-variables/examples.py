a1ea1d49095475336c77214a7065bc41b7c0eeb0
Filipe Chau <filipechau.dev@gmail.com>
2025-06-10 20:07:58 +0200

    add examples of float variables to example.py

--- 01-variables/examples.py
+++ 01-variables/examples.py
@@ -6,14 +6,16 @@
 first_name = "Filipe"
 last_name = "Chau"
 email = "chaulee@real.com"
-print(f"Hello, {first_name} {last_name}!")
-print(f"Your email address is: {email}")
+
 
 # Integers
 age = 25
 num_of_students = 30
 quantity = 4
 
-print(f"You are {age} years old")
-print(f"You are buying {quantity} items")
-print(f"Your class has {num_of_students} students")
+# Float
+price = 17.50
+gpa = 5.75
+distance = 100.5
+print(f"The price is {price} and the quantity is {quantity}")
+print(f"{first_name} {last_name} is {age} years old and has a GPA of {gpa}")
