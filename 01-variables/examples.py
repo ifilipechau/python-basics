fe913236c3e9ff7a17c6d1ff283dcbddbf722abd
Filipe Chau <filipechau.dev@gmail.com>
2025-06-10 13:49:12 +0200

    add notes.md for variables and create examples.py

---
+++ 01-variables/examples.py
@@ -0,0 +1,11 @@
+# Variables 
+
+
+# The variables below are called strings, they are being used with an f string
+
+first_name = "Filipe"
+last_name = "Chau"
+email = "chaulee@real.com"
+print(f"Hello, {first_name} {last_name}!")
+print(f"Your email address is: {email}")
+
