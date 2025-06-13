409948400ec58d3a4790e6367cd52cb0889ea1d9
Filipe Chau <filipechau.dev@gmail.com>
2025-06-13 17:52:37 +0200

    add name input handling

--- 05-if-statements/examples.py
+++ 05-if-statements/examples.py
@@ -1,9 +1,18 @@
 # if-else = Do some code IF some condition is TRUE, otherwise do some other code
 
-
+'''
 age = int(input('Enter your age:'))
 
 if age >= 18:
     print('You are now signed up!')
 else:
     print('You must be 18+ to sign up!')
+
+'''
+
+name = input("Enter your name: ")
+
+if name == "":
+    print("You didn't enter a name!")
+else:
+    print(f"Hello {name}!")
