83aab43985d73a89abc3cd171a24e9478435adb6
Filipe Chau <filipechau.dev@gmail.com>
2025-06-13 12:34:58 +0200

    add examples arithmetic operators in python

---
+++ 04-maths/examples.py
@@ -0,0 +1,33 @@
+# Arithmetic operators
+
+friends = 0
+# Addition
+# friends = friends + 1
+# friends += 1 # this is argumented assign operator
+
+# Multiplication
+# friends = friends * 3
+# friends *= 3
+
+# Subtraction
+# friends = friends - 2
+# friends -= 2
+
+# Division
+# friends = friends / 2
+# friends /= 2
+
+# Exponentiation
+# friends = friends ** 2
+# friends **= 2
+
+# Modulus
+# friends = friends % 3
+# friends %= 3
+
+# Floor division
+# friends = friends // 2
+# friends //= 2
+
+# Print the result
+# print(friends)
