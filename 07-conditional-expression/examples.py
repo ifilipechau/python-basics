77dea6c7130aab0abd39173726be7f41846e7737
Filipe Chau <filipechau.dev@gmail.com>
2025-06-13 20:47:27 +0200

    refactor: update examples to include max number calculation using ternary operator

--- 07-conditional-expression/examples.py
+++ 07-conditional-expression/examples.py
@@ -3,7 +3,19 @@
 #                          X if condition else Y
 
 num = 7
+a = 9
+b = 10
+age = 16
 
 # print("Positive" if num > 0 else "Negative or Zero")
-result = "EVEN" if num % 2 == 0 else "ODD"
-print(result)
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# print(result)
+
+# max_num = a if a > b else b
+# print(max_num)
+
+# min_num = a if a < b else b
+# print(min_num)
+
+status = "You are an adult" if age >= 18 else "You are just a kid"
+print(status)
