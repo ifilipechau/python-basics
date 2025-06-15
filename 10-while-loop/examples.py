fcaa206446441b0346883830b97a60c487a629d7
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 14:42:26 +0200

    add: enhance examples with additional comments for clarity on while loop usage

--- 10-while-loop/examples.py
+++ 10-while-loop/examples.py
@@ -4,6 +4,8 @@
 name = input("Enter your name: ")
 
 '''
+# Example - Using if
+
 if name == "":
     print("You did not enter your name")
 else:
@@ -11,8 +13,22 @@ else:
 
 '''
 
+'''
+Example 1 - Using the previous examples but implement it with while loop
+
 while name == "":
     print("You did not enter your name")
     name = input("Enter your name: ")
 
 print(f"Your name is {name}")
+
+'''
+# Example 2
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")
