a3c48d234be17c5d2cb23284935d1b88a139626f
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 16:32:20 +0200

    add: implement countdown timer with user-defined duration

---
+++ 11-for-loops/exercises-01.py
@@ -0,0 +1,18 @@
+# Count down timer
+
+import time
+
+my_time = int(input("Enter the time in seconds: "))
+'''
+for x in range(0, my_time): # range starts at 0 and goes to my_time - 1
+    print(x)
+    time.sleep(5) # Works in seconds, so 5 means 5 seconds 
+
+print("TIME'S UP")
+'''
+# This is the countdown timer
+for x in range(my_time, 0, -1): # range starts at my_time and goes to 1
+    print(x)
+    time.sleep(1) # Works in seconds, so 1 means 1 second
+    
+print("TIME'S UP")
