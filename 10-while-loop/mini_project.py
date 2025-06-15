56f3d3f4306943e2022200889c963698d33024bb
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 15:14:39 +0200

    add: implement input validation for interest rate and time period in compound interest calculator

--- 10-while-loop/mini_project.py
+++ 10-while-loop/mini_project.py
@@ -10,4 +10,12 @@ while principal <= 0:
     if principal <= 0:
         print("Principal can't be less than or equal to zero")
 
-print(principal)
+while rate <= 0:
+    rate = float(input("Enter the interest rate: "))
+    if rate <= 0:
+        print("Interest rate can't be less than or equal to zero")
+
+while time <= 0:
+    time = int(input("Enter time in years: "))
+    if time <= 0:
+        print("Time can't be less than or equal to zero")
