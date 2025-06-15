102487744528de0fae00c2af5b90364c2b3bc05a
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 14:44:28 +0200

    fix: update example 1 comment and reposition name input for clarity

--- 10-while-loop/examples.py
+++ 10-while-loop/examples.py
@@ -1,10 +1,10 @@
 # While loop = execute a block of code as long as a condition is True
 
-# Example 1: Basic while loop
-name = input("Enter your name: ")
 
 '''
 # Example - Using if
+
+name = input("Enter your name: ")
 
 if name == "":
     print("You did not enter your name")
