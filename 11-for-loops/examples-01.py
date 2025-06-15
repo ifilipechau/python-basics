21f771d5526f4d192acabc3ea3c2c3e8a5d20e9e
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 16:00:37 +0200

    add: create second for loops example with range and continue statement

---
+++ 11-for-loops/examples-01.py
@@ -0,0 +1,14 @@
+# For loops example 2
+
+'''
+credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+    print(x)
+'''
+
+for x in range(1, 21):
+    if x == 13:
+        continue
+    else:
+        print(x)
