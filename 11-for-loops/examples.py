4d1610ef47e909ce8c0881fa2a3eccb1a0dff451
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 15:47:45 +0200

    add: examples to demonstrate for loops

---
+++ 11-for-loops/examples.py
@@ -0,0 +1,7 @@
+# for loops = execute a block of code a fixed number of times.
+#               you can iterate over a range, string, sequence, etc.
+
+for x in reversed(range(1, 5)):
+    print(x)
+
+print( "Happy new year!")
