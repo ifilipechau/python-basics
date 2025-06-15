7f636838e562063d85083b64eb90d4f596382998
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 16:04:26 +0200

    fix: add comments for clarity in for loop example

--- 11-for-loops/examples-01.py
+++ 11-for-loops/examples-01.py
@@ -7,8 +7,8 @@ for x in credit_card:
     print(x)
 '''
 
-for x in range(1, 21):
+for x in range(1, 21): # count from 1 to 20 
     if x == 13:
-        continue
-    else:
+        continue # skip the number 13 and continue to the next iteration, 
+    else:        # we could also use 'break' instead of 'continue' to stop the loop (in 12th iteration)
         print(x)
