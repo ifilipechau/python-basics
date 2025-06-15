6a1e4bb83d60d2fc3648d9fb4373f3451dc63e64
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 16:37:56 +0200

    fix: update countdown timer to display time in HH:MM:SS format

--- 11-for-loops/exercises-01.py
+++ 11-for-loops/exercises-01.py
@@ -12,7 +12,10 @@ print("TIME'S UP")
 '''
 # This is the countdown timer
 for x in range(my_time, 0, -1): # range starts at my_time and goes to 1
-    print(x)
+    seconds = x % 60 # Get the seconds part
+    minutes = int(x / 60) % 60 # Get the minutes part
+    hours = int(x / 3600) # Get the hours part
+    print(f"{hours:02}:{minutes:02}:{seconds:02}") # Print in HH:MM:SS format
     time.sleep(1) # Works in seconds, so 1 means 1 second
-    
+
 print("TIME'S UP")
