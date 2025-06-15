99a69dd424b2086562673930a068cb3298ab4fd7
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 15:53:03 +0200

    fix: correct order of for loops and add missing print statements in examples

--- 11-for-loops/examples.py
+++ 11-for-loops/examples.py
@@ -1,7 +1,15 @@
 # for loops = execute a block of code a fixed number of times.
 #               you can iterate over a range, string, sequence, etc.
 
-for x in reversed(range(1, 5)):
+for x in (range(1, 5)): # count from 1 to 4
+    print(x)
+print("**************************")
+
+for x in reversed(range(1, 4)): # count from 3 to 1
     print(x)
 
 print( "Happy new year!")
+
+print("**************************")
+for x in (range(1, 10, 2)): # count from 1 to 9, skipping every second number
+    print(x)
