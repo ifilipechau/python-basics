be67fdd4a9b8b5c73bcd10a4ebb8413dccdcc4b8
Filipe Chau <filipechau.dev@gmail.com>
2025-06-15 14:33:37 +0200

    add: implement basic while loop example for user input validation

---
+++ 10-while-loop/examples.py
@@ -0,0 +1,18 @@
+# While loop = execute a block of code as long as a condition is True
+
+# Example 1: Basic while loop
+name = input("Enter your name: ")
+
+'''
+if name == "":
+    print("You did not enter your name")
+else:
+    print(f"Hello {name}")
+
+'''
+
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name")
+
+print("Your name is {name}")
