9aa6d60b8c958c1e9c5eb989984114631062aaac
Filipe Chau <filipechau.dev@gmail.com>
2025-06-16 21:23:14 +0200

    fix: uncomment membership check in fruits list and add examples for set operations

--- 13-collection/set-examples.py
+++ 13-collection/set-examples.py
@@ -1,5 +1,15 @@
 # set = {} unordered and immutable, but Add/Remove OK. NO duplicates
 
 fruits = {"apple", "orange", "banana", "coconut"}
+# fruits.add("kiwi") # add an item to the set
+# fruits.remove("banana") # remove an item from the set
+# fruits.pop()
+fruits.clear() # clear the set, removing all items
 
 print(fruits) # print the set
+# print(dir(fruits))  # we use dir() to see the methods available for the set
+# print(help(fruits)) # we use help() to see the documentation for the set methods
+# print(len(fruits))  # returns the number of items in the set
+# print("apple" in fruits)  # check if an item is in the set
+
+
