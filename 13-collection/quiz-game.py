e4c862e30cd53a8c9a31d34f5053916d81a9ee34
Filipe Chau <filipechau.dev@gmail.com>
2025-06-18 20:41:59 +0200

    add: define options for quiz questions in a 2D tuple

--- 13-collection/quiz-game.py
+++ 13-collection/quiz-game.py
@@ -5,7 +5,12 @@ questions = ("How many elements are in the periodic table?: ",
              "What is the most abudant gas in the Earth's atmosphere?: ",
              "How many bones are in the human body?: ",
              "Which planet in the solar system is the hottest?: ") # tuple of questions
-options = () # tuple of options
+options = (("A. 116", "B. 117", "C. 118", "D. 119"), 
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"), 
+           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"), 
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars")) # We've 4 options for every questions that's why we're using 2D tuple
+
 answers = () # tuple of answers
 guesses = [] # list of guesses
 score = 0
