c30ee0c572dfcedaaad455f110b8ec00aa972cf7
Filipe Chau <filipechau.dev@gmail.com>
2025-06-18 20:33:20 +0200

    add: initialize quiz game with questions and data structures

---
+++ 13-collection/quiz-game.py
@@ -0,0 +1,12 @@
+# Python quiz game
+
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abudant gas in the Earth's atmosphere?: ",
+             "How many bones are in the human body?: ",
+             "Which planet in the solar system is the hottest?: ") # tuple of questions
+options = () # tuple of options
+answers = () # tuple of answers
+guesses = [] # list of guesses
+score = 0
+question_num = 0
