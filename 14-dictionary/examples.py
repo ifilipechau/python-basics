03299d1e7e073784a97d710847ff2fff324a2843
Filipe Chau <filipechau.dev@gmail.com>
2025-06-19 19:43:49 +0200

    comment out unused code and improve readability

--- 14-dictionary/examples.py
+++ 14-dictionary/examples.py
@@ -7,7 +7,35 @@ capitals = {"USA": "Washington D.C.",
             "Russia": "Moscow"}
 
 
-print(dir(capitals)) # to see all the methods and attributes of the dictionary
-print(help(capitals)) # To see all the descriptions of the attributes and methods
+# print(dir(capitals)) # to see all the methods and attributes of the dictionary
+# print(help(capitals)) # To see all the descriptions of the attributes and methods
 
-print(capitals.get("USA"))
+# print(capitals.get("USA"))
+
+# Check if a key is within a dictionary
+
+# if capitals.get("Japan"):
+#    print("That capital exist")
+#else:
+#    print("That capital doesn't exist")
+
+    # To insert a new value key-pair or update the existing ones
+# capitals.update({"Mozambique": "Maputo"})
+# capitals.update({"USA": "New York"})
+# print(capitals)
+
+    # To remove a key-pair value
+# capitals.pop("China")
+# print(capitals)
+
+# capitals.popitem()
+# capitals.clear()
+
+# keys = capitals.keys()
+# for key in capitals.keys():
+#   print(key)
+
+values = capitals.values()
+
+for value in capitals.values():
+    print(value, end = " ")
