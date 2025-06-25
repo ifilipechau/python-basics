feeb1db156fd918d86494b29ae361e2474ed6151
ifilipechau <filipechau.dev@gmail.com>
2025-06-25 06:25:25 +0200

    add: implement a for loop for printing dice_art in a row line

--- small-projects/dice-rolling.py
+++ small-projects/dice-rolling.py
@@ -50,7 +50,10 @@ num_of_dice = int(input(("How many dice?: ")))
 for die in range(num_of_dice):
     dice.append(random.randint(1, 6))
 
-
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line])
+        
 for die in dice:
     total += die
 print(f"total: {total}")
