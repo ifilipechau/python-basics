71905c2ae2e95cd89008284b591c88b141972672
ifilipechau <filipechau.dev@gmail.com>
2025-06-25 06:28:11 +0200

    add: implement the end= prop and and update dice art printing to display in a single line

--- small-projects/dice-rolling.py
+++ small-projects/dice-rolling.py
@@ -52,8 +52,9 @@ for die in range(num_of_dice):
 
 for line in range(5):
     for die in dice:
-        print(dice_art.get(die)[line])
-        
+        print(dice_art.get(die)[line], end="")
+      print()
+
 for die in dice:
     total += die
 print(f"total: {total}")
